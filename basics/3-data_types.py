ae6459e30a233b20e0a5aaa1573de0080d817c4c
Mohamed Hibbi <medhibbi7@gmail.com>
2026-02-09 11:37:25 +0100

    adding the Data type file

---
+++ basics/3-data_types.py
@@ -0,0 +1,9 @@
+# Checking data types
+
+print(type(1928))                  # Int
+print(type(10.018))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Mohamed'))          # String
+print(type([8, 2, 3, 10]))           # List
+print(type({'name': 'Mohamed'}))  # Dictionary
+print(type({10.8, 2.4, 7.7}))    # Tuple
